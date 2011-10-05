b5f4b8cf4264d3d6759070ee9744a96271cf48a9
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2011-10-05 22:10:36 +0200

    [OPENENGSB-1851] replaced DefaultJPADao with QueryDslJPADao

--- components/edb/src/main/java/org/openengsb/core/edb/internal/JPADatabase.java
+++ components/edb/src/main/java/org/openengsb/core/edb/internal/JPADatabase.java
@@ -42,8 +42,8 @@ import org.openengsb.core.api.edb.EDBObject;
 import org.openengsb.core.api.edb.EDBUpdateEvent;
 import org.openengsb.core.api.model.OpenEngSBModel;
 import org.openengsb.core.api.model.OpenEngSBModelEntry;
-import org.openengsb.core.edb.internal.dao.DefaultJPADao;
 import org.openengsb.core.edb.internal.dao.JPADao;
+import org.openengsb.core.edb.internal.dao.QueryDslJPADao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -545,6 +545,6 @@ public class JPADatabase implements org.openengsb.core.api.edb.EngineeringDataba
 
     public void setEntityManager(EntityManager entityManager) {
         this.entityManager = entityManager;
-        dao = new DefaultJPADao(entityManager);
+        dao = new QueryDslJPADao(entityManager);
     }
 }
